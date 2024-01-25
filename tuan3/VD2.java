448af759149b216aad456a6de32810af3961511e
maulan1208 <nguyenlan12082003@gmail.com>
2024-01-25 18:08:20 +0700

    tuan4

--- tuan3/VD2.java
+++ tuan3/VD2.java
@@ -4,7 +4,6 @@ package com.mycompany.project1.tuan3;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-
 public class VD2 {
     private int count = 0;
     private final Lock lc = new ReentrantLock();
--- tuan3/VD3.java
+++ tuan3/VD3.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.project1.tuan3;
 
-/**
- *
- * @author lenovo
- */
 public class VD3 {
     private int count = 0;
     public synchronized void increment(){
