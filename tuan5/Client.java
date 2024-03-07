3fc0906193d84c8f1bbc1c329a887fd973f15587
maulan1208 <nguyenlan12082003@gmail.com>
2024-03-07 21:29:54 +0700

    tuan7

--- tuan5/Client.java
+++ tuan5/Client.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        final String SERVER_ADDRESS = "192.168.0.124";
+        final String SERVER_ADDRESS = "192.168.236.194";
         final int PORT = 12345;
         try {
             Socket socket = new Socket(SERVER_ADDRESS, PORT);
